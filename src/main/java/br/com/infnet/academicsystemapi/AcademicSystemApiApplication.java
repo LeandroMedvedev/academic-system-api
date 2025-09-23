6883319022e2aa8dbb4e750b0391804e32e31dc9
LeandroMedvedev <medvedevleandro@outlook.com>
2025-09-23 11:26:15 -0300

    feat: initial project structure with core dependencies

---
+++ src/main/java/br/com/infnet/academicsystemapi/AcademicSystemApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.infnet.academicsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicSystemApiApplication.class, args);
+	}
+
+}
